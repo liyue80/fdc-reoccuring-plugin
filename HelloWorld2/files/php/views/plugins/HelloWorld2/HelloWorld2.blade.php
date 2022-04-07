3676106e965132ad088cb4a1659725572142998c
liyue <liyue80@gmail.com>
2022-04-08 01:32:49 +0800

    自动格式化PHP文件

--- HelloWorld2/files/php/views/plugins/HelloWorld2/HelloWorld2.blade.php
+++ HelloWorld2/files/php/views/plugins/HelloWorld2/HelloWorld2.blade.php
@@ -1,14 +1,11 @@
 <style>
 </style>
-<div class="modal fade" id="HelloWorld2Dialog" tabindex="-1" role="dialog" style="width:95%; height:95%;"
-	aria-labelledby="modalLabel" aria-hidden="true" data-backdrop="static">
+<div class="modal fade" id="HelloWorld2Dialog" tabindex="-1" role="dialog" style="width:95%; height:95%;" aria-labelledby="modalLabel" aria-hidden="true" data-backdrop="static">
 	<div class="modal-dialog ask-dialog" style="max-width: 90%; height: 85%">
 		<div class="modal-content" style="height:100%;overflow: visible;">
 			<div class="modal-header">
 				<h5 class="modal-title" id="modalLabel">Hello World 2</h5>
-				<button type="button"
-					class="close push-xs-right form-control input-sm"
-					data-dismiss="modal" aria-label="Close">
+				<button type="button" class="close push-xs-right form-control input-sm" data-dismiss="modal" aria-label="Close">
 					<span aria-hidden="true">&times;</span>
 				</button>
 			</div>
@@ -21,42 +18,43 @@
 
 @push('scripts')
 <script>
-//IMPORTANT - csrf_token
-$.ajaxSetup({
-    headers: { 'X-CSRF-TOKEN': '{{ csrf_token() }}' },
-    error: function(jqXHR, textStatus, errorThrown)
-    {
-        toastr.error('Lost Connection');
-    }
-});
+	//IMPORTANT - csrf_token
+	$.ajaxSetup({
+		headers: {
+			'X-CSRF-TOKEN': '{{ csrf_token() }}'
+		},
+		error: function(jqXHR, textStatus, errorThrown) {
+			toastr.error('Lost Connection');
+		}
+	});
 
 
-var filePath = '';
-$(document).ready(function() {
-	
-	fillUser2Name();
-	
-	function fillUser2Name() {
-		var success = false;
-		var formData = new FormData();
-		formData.append('accountId', 2);
-		$.ajax({			
-	        url: '{{ url("/helloWorld2/getUserNameById") }}',
-	        type: 'POST',
-            cache: false,
-            data: formData,
-            processData: false,
-            contentType: false,
-	        success: function(data){
-		        console.log(data);
-		        $('#user2Name').text('Hello!!! ' + data.toUpperCase());
-	        },	
-	        error: function(jqXHR, textStatus, errorThrown)  {
-	            console.log(errorThrown);
-	        },
-	    }); 
-	};
-  
-});
+	var filePath = '';
+	$(document).ready(function() {
+
+		fillUser2Name();
+
+		function fillUser2Name() {
+			var success = false;
+			var formData = new FormData();
+			formData.append('accountId', 2);
+			$.ajax({
+				url: '{{ url("/helloWorld2/getUserNameById") }}',
+				type: 'POST',
+				cache: false,
+				data: formData,
+				processData: false,
+				contentType: false,
+				success: function(data) {
+					console.log(data);
+					$('#user2Name').text('Hello!!! ' + data.toUpperCase());
+				},
+				error: function(jqXHR, textStatus, errorThrown) {
+					console.log(errorThrown);
+				},
+			});
+		};
+
+	});
 </script>
 @endpush
