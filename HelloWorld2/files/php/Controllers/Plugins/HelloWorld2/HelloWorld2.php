baec028c2c9c8c918afacc88165113f0451091e8
liyue <liyue80@gmail.com>
2022-04-08 13:00:03 +0800

    格式化PHP文件

--- HelloWorld2/files/php/Controllers/Plugins/HelloWorld2/HelloWorld2.php
+++ HelloWorld2/files/php/Controllers/Plugins/HelloWorld2/HelloWorld2.php
@@ -7,25 +7,27 @@ use Illuminate\Http\Request;
 use Ixudra\Curl\Facades\Curl;
 use Log;
 
-class HelloWorld2 extends Controller {
-	
-public function getUserNameById(Request $request){
-	    $accountId = $request->input('accountId');
-	    Log::info('accountId = ' . $accountId);
-	    $response = Curl::to($this->apiUrl . "/ainvr/api/accounts/{$accountId}")
-            	    ->withHeader('x-auth-token:' . $this->token)
-            	    ->withOption('SSL_VERIFYHOST', false)
-            	    ->returnResponseObject()
-            	    ->get();
-	         	
-	    if ($response->status != 200){
-	    	$libs = new CommonLib();
+class HelloWorld2 extends Controller
+{
+
+	public function getUserNameById(Request $request)
+	{
+		$accountId = $request->input('accountId');
+		Log::info('accountId = ' . $accountId);
+		$response = Curl::to($this->apiUrl . "/ainvr/api/accounts/{$accountId}")
+			->withHeader('x-auth-token:' . $this->token)
+			->withOption('SSL_VERIFYHOST', false)
+			->returnResponseObject()
+			->get();
+
+		if ($response->status != 200) {
+			$libs = new CommonLib();
 			return $libs->handelHttpReturnMessage($response->content);
-	    }
-            	        
-	    //$ret['status'] = $response->status;
-	    $userName = json_decode($response->content)->name;
-	    
-	    return $userName;
+		}
+
+		//$ret['status'] = $response->status;
+		$userName = json_decode($response->content)->name;
+
+		return $userName;
 	}
 }
